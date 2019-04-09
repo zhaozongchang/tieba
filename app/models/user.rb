59f0edb732fe901543d93ea9a216470e4d331040
zhaozongchang <xingyingsuifeng@gmail.com>
2019-04-09 21:57:54 +0800

    添加评论功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :topics
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   root 'topics#index'
 
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
     put "like",    to: "topics#upvote"
     put "dislike", to: "topics#downvote"
   end
+  resources :comments
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
